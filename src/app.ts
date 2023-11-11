ce64cf20561ecbd9685a6b36471bdf9ffaca31ee
Filipe Campos <lip3.campos@gmail.com>
2023-11-11 19:43:00 -0300

    feat: get method

--- src/app.ts
+++ src/app.ts
@@ -9,13 +9,29 @@ app.use(cors({ origin: '*' }))
 app.use(json({ limit: '50mb' }))
 app.use(urlencoded({ limit: '50mb', extended: true }))
 
+/*
+
+querido guts, pai não sabia onde encaixar as requisições
+então acabei fazendo aqui mesmo.
+
+se tiver um local específico, fique a vontade para me
+ensinar, ou apenas mudar e me avisar depois :)
+
+API - SRCE:
+
+*/
+
 app.get('/', (req: Request, res: Response) => {
 	res.status(200).json({
 		name: 'UPX API',
 		version: process.env.npm_package_version,
 		env: process.env.ENVIRONMENT,
+		message: 'yeah buddy! its working!'
 	})
+	
 })
+
+// outros métodos abaixo...
 
 app.use('/', routes)
 
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import consoleColors from './helpers/consoleColors'
 
 const port = process.env.PORT
 
+
+
 app.listen(port, () => {
 	console.log(
 		consoleColors.Green,
